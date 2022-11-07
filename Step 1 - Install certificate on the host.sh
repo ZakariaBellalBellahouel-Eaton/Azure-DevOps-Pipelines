ee59aacff82a51edb5ad51485271763d1fcdc3b1
Zakaria Bellal Bellahouel <ZakariaBellalBellahouel@eaton.com>
2022-11-07 17:39:07 -0500

    Add sudo to step 1

--- Step 1 - Install certificate on the host.sh
+++ Step 1 - Install certificate on the host.sh
@@ -7,11 +7,11 @@ apt upgrade -y
 
 # Check if sudo lib is installed, if not, install it
 if [[ ! $(dpkg -l ca-certificates | grep ii 2>/dev/null) ]]; then
-    apt install ca-certificates -y
+    sudo apt install ca-certificates -y
 fi
 
 # Install caCopy the Eaton certificate and loadit
 sudo cp $EATONCERTIFICATE_SECUREFILEPATH  /usr/local/share/ca-certificates/
 
 # Update the certificate
-update-ca-certificates
+sudo update-ca-certificates
